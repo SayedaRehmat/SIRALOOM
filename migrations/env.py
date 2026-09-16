7e115b74d6b4dc9b0e5c392d4251d5a36899b2cb
SayedaRehmat <bc190408541@vu.edu.pk>
2026-09-16 12:27:44 +0500

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ import sys
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-ROOT = Path(**file**).resolve().parents[1]
+ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
 sys.path.insert(0, str(ROOT))
 
